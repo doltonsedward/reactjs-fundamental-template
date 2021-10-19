e691f4f19a347f93ae86ac8be7a0a45b44e90209
yosep <yosepalexsander9@gmail.com>
2021-10-19 11:41:39 +0700

    done conditional rendering

--- src/conditionRendering.js
+++ src/conditionRendering.js
@@ -1,15 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 //Create Component PrivatePage
-
-//Create Component GuestPage
-
-function ConditionRenderin() {
-  //init State
+function PrivatePageAdmin(props) {
   return (
-    // Code Inside div
-    <div>{/* conditional logic rendering */}</div>
+    <>
+      <h1>Hello Admin Spongebob</h1>
+      <img src="https://www.greenscene.co.id/wp-content/uploads/2021/01/spongebob-1-696x497.jpg" alt="spongebob" />
+      <button onClick={props.onSignout}>Sign Out</button>
+    </>
   );
 }
 
-export default ConditionRenderin;
+function PrivatePageUser(props) {
+  return (
+    <>
+      <h1>Hello User Patrick</h1>
+      <img src="https://www.greenscene.co.id/wp-content/uploads/2021/01/spongebob-1-696x497.jpg" alt="spongebob" />
+      <button onClick={props.onSignout}>Sign Out</button>
+    </>
+  );
+}
+
+//Create Component GuestPage
+function GuestPage(props) {
+  return (
+    <>
+      <h1>Please Signin</h1>
+      <button onClick={props.onSignin}>Sign In</button>
+    </>
+  );
+}
+
+function ConditionRendering() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(true);
+
+  const handleSignin = () => {
+    setIsLoggedIn(true);
+  };
+  const handleSignout = () => {
+    setIsLoggedIn(false);
+  };
+
+  const renderPage = () => {
+    if (isLoggedIn) {
+      if (isAdmin) {
+        return <PrivatePageAdmin onSignout={handleSignout} />;
+      } else {
+        return <PrivatePageUser onSignout={handleSignout} />;
+      }
+    } else {
+      return <GuestPage onSignin={handleSignin} />;
+    }
+  };
+
+  return (
+    // Code Inside div
+    <div className="app">
+      {isLoggedIn ? (
+        <>{isAdmin ? <PrivatePageAdmin onSignout={handleSignout} /> : <PrivatePageUser onSignout={handleSignout} />}</>
+      ) : (
+        <GuestPage onSignin={handleSignin} />
+      )}
+    </div>
+  );
+}
+
+export default ConditionRendering;
