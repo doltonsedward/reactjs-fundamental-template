459c62911ac152808763096e1ab677b38dba7a92
yosep <yosepalexsander9@gmail.com>
2021-10-19 09:46:23 +0700

    done embedding expression in JSX

--- src/embedExpression.js
+++ src/embedExpression.js
@@ -1,14 +1,35 @@
-import React from "react";
-
+// embedding expression in JSX using functional way
 function EmbedExpression() {
-  //Create function here
+  const companyName = "Dumbways";
 
-  //Create a variable here
+  const getBatch = (value) => {
+    return value;
+  };
 
   return (
-    // Code Inside div
-    <div></div>
+    <div>
+      Welcome to {companyName} batch {getBatch(27)}
+    </div>
   );
+}
+
+// embedding expression in JSX using class way
+import React, { Component } from "react";
+
+class EmbedExpression extends Component {
+  getBatch(value) {
+    return value;
+  }
+
+  render() {
+    const companyName = "Dumbways";
+
+    return (
+      <div>
+        Welcome to {companyName} batch {this.getBatch(27)}
+      </div>
+    );
+  }
 }
 
 export default EmbedExpression;
