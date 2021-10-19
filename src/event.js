26ce5a4b80b65cebd02587086509dca052ce2eae
yosep <yosepalexsander9@gmail.com>
2021-10-19 09:57:16 +0700

    done handling event

--- src/event.js
+++ src/event.js
@@ -1,11 +1,27 @@
 import React from "react";
 
 function Event() {
-  //Create function here
+  const getBatch = (value) => {
+    return value;
+  };
+  const companyName = "Dumbways.id";
+
+  const greetings = () => {
+    alert(`Hello Bootcamp Student Batch ${getBatch(27)}`);
+  };
+
+  const spyChange = (e) => {
+    console.log("change input with value", e.target.value);
+  };
 
   return (
-    // Code Inside div
-    <div></div>
+    <div>
+      <p>
+        Welcome to {companyName} Batch {getBatch(27)}
+      </p>
+      <button onClick={greetings}>Click Me</button>
+      <input onChange={spyChange}></input>
+    </div>
   );
 }
 
